354736478d5d2f7e72f91a86372642efea297815
RECAP <master@MacBook-Pro.local>
2026-05-19 21:04:55 +0530

    config: configure images qualities in next.config.ts to resolve quality warnings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    qualities: [40, 60, 65, 70, 75],
+  },
 };
 
 export default nextConfig;
